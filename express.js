7427c40c5298653a31aec51294be7132c44247c4
Alex Somoza <asomoza041@west-mec.org>
2019-12-09 18:27:19 -0700

    New stuff
    Stuff

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var fetch = require('node-fetch');
 var path = require('path');
-var boot = require('bootstrap');
+// var boot = require('bootstrap')
 
 
 
@@ -12,55 +12,53 @@ app.set("views", path.join(__dirname, "views"));
 
 // app.get('/poke',(req,res)=>res.send("something"))
 
-app.get('/poke/:pokemonName', async(req, response) => {
+// app.get('/poke/:pokemonName', async(req, response) => {
     
-    try{
-        pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${req.params.pokemonName}`);
+//     try{
+//         pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${req.params.pokemonName}`);
 
-        const json = await pokemonData.json();
-        console.log(json.sprites.back_default);
+//         const json = await pokemonData.json();
+//         console.log(json.sprites.back_default);
 
-        const {...sprites} =json.sprites;
-        console.log("sprites", sprites);
+//         const {...sprites} =json.sprites;
+//         console.log("sprites", sprites);
 
-        response.render('poke', {
-            name: json.name,
-            img: sprites.front_default
-        })
-    } catch(error) {
-        console.log(error)
-    }
+//         response.render('poke', {
+//             name: json.name,
+//             img: sprites.front_default
+//         })
+//     } catch(error) {
+//         console.log(error)
+//     }
 
-});
-
-// app.get('/', async (req, res) => {
-//     const mudkipFetch = await fetch('https://pokeapi.co/api/v2/pokemon/mudkip');
-//     mudkip = await mudkipFetch.json();
-
-//     const cuboneFetch = await fetch('https://pokeapi.co/api/v2/pokemon/cubone');
-//     cubone = await cuboneFetch.json();
-
-//     const squirtleFetch = await fetch('https://pokeapi.co/api/v2/pokemon/squirtle');
-//     squirtle = await squirtleFetch.json();
-
-//     const snorlaxFetch = await fetch('https://pokeapi.co/api/v2/pokemon/snorlax');
-//     snorlax = await snorlaxFetch.json();
-    
-    
-//     res.render('index', {
-//         title: "Home",
-//         mudkipData: mudkip,
-//         cuboneData: cubone,
-//         squirtleData: squirtle,
-//         snorlaxData: snorlax
-//     })
 // });
-
 app.get('/', (req, response) => {
     response.render('index', {
-    title: "Pokémon Fandom"
+    title: "Home"
     });
 });
+
+app.get('/poke', async (req, response) => {
+    const pikaFetch = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
+    pika = await pikaFetch.json();
+
+
+    response.render('poke', {
+        title: "Pokémon Fandom",
+        pikaData: pika,
+    })
+});
+
+app.get('/potter', async (req, response) => {
+    const sortingHat = await fetch('https://www.potterapi.com/v1/sortingHat');
+    sort = await sortingHat.json();
+
+    response.render('poke', {
+        title: "Harry Potter Fandom",
+        hatData: sort
+    })
+});
+
 
 
 app.listen(3000)
